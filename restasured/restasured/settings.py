814008ab78a590a101777b6ae23eff4dddf7af9e
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2023-02-22 19:03:45 +0100

    adding the guest ip to allowed hosts

--- restasured/restasured/settings.py
+++ restasured/restasured/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+=!nba$5*xvimxqk(2^#!+i+y=lv#%jb*qv3&%a$=8r%e6o1q%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.64.3']
 
 
 # Application definition
